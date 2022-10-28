d446d3379aabe2d312a975d0f65e767c593c8808
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2022-10-28 21:14:48 +0900

    make painting board

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,27 +1,62 @@
+const color = document.getElementById("color")
 const canvas = document.querySelector("canvas")
 const cxt = canvas.getContext("2d")
+const lineWidth = document.getElementById("line-width")
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.getElementById("mode-Btn")
 
 canvas.height = 600;
 canvas.width = 600;
+cxt.lineWidth = lineWidth.value;
+let isPainting = false;
+let isFilling= false;
 
-cxt.fillRect (200, 200, 50, 100);
-cxt.fillRect (275, 200, 100, 300);
-cxt.fillRect (400, 200, 50, 100);
+function onMove(event) {
+    if (isPainting) {
+        cxt.lineTo(event.offsetX, event.offsetY);
+        cxt.stroke();
+        return;
+    }
+    cxt.beginPath();
+    cxt.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+    isPainting = true;
+}
+function canclePainting() {
+    isPainting = false;
+}
+function onLineWidthChange(event) {
+    cxt.lineWidth = event.target.value;
+    console.log(event.target.value);
+}
+function onColorChange(event) {
+    cxt.strokeStyle = event.target.value;
+    cxt.fillStyle = event.target.value;
+}
+function onColorClick(event) {
+    const colorValue = event.target.dataset.color;
+    cxt.strokeStyle = colorValue;
+    cxt.fillStyle = colorValue;
+    color.value = colorValue;
+}
+function onModeClick(){
+    if (isFilling){
+        isFilling = false
+        modeBtn.innerText = "DRAW";
+    } else {
+        isFilling = true
+        modeBtn.innerText = "FILL";
+    }
+}
 
-cxt.arc(325, 100, 75, 0, Math.PI * 2);
-cxt.fill();
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", canclePainting);
+canvas.addEventListener("mouseleave", canclePainting);
 
-cxt.beginPath ();
-cxt.arc(300, 75, 10, Math.PI, Math.PI * 2);
-cxt.fillStyle = "yellow";
-cxt.fill();
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
 
-cxt.beginPath ();
-cxt.arc(350, 75, 10, Math.PI, Math.PI * 2);
-cxt.fillStyle = "blue";
-cxt.fill();
-
-cxt.beginPath ();
-cxt.arc(325, 100, 20, 0, Math.PI * 1);
-cxt.fillStyle = "green";
-cxt.fill();
+modeBtn.addEventListener("click", onModeClick);
